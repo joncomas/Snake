1de61b53845fc0d2d878c4bf7d8b8e9e4e15111e
joncomas <joncomas@outlook.com>
2020-04-23 01:53:09 -0400

    Fixed third argument in main

--- src/draw.rs
+++ src/draw.rs
@@ -1,17 +1,21 @@
-use::piston_window::{rectagle, Context, G2d};
-use::piston_window::types::Color;
+use piston_window::{rectangle, Context, G2d};
+use piston_window::types::Color;
 
 const BLOCK_SIZE: f64 = 25.0;
 
-oub fn to_coord(game_coord: i32) -> f64 {
+pub fn to_coord(game_coord: i32) -> f64 {
     (game_coord as f64) * BLOCK_SIZE
 }
 
-pub fn draw_block(color: Color, x: i32, y: i32, con: &Context, g: &mut G2d ) {
+pub fn to_coord_u32(game_coord: i32) -> u32 {
+    to_coord(game_coord) as u32
+}
+
+pub fn draw_block(color: Color, x: i32, y: i32, con: &Context, g: &mut G2d) {
     let gui_x = to_coord(x);
     let gui_y = to_coord(y);
 
-    rectagle(
+    rectangle(
         color,
         [gui_x, gui_y, BLOCK_SIZE, BLOCK_SIZE],
         con.transform,
@@ -26,18 +30,18 @@ pub fn draw_rectangle(
     width: i32,
     height: i32,
     con: &Context,
-    g: &mut: G2d,
+    g: &mut G2d,
 ) {
     let x = to_coord(x);
     let y = to_coord(y);
 
-    rectagle(
+    rectangle(
         color,
         [
             x,
             y,
             BLOCK_SIZE * (width as f64),
-            BLOCK_SIZE * (height as f64)
+            BLOCK_SIZE * (height as f64),
         ],
         con.transform,
         g,
